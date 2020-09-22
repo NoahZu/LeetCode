88d1a66d5df76108085a990e30a3102914ee4f57
zujinhao <smallzoo@foxmail.com>
2020-09-22 20:01:39 +0800

    update 9.22

--- src/hash相关/q387_字符串中的第一个唯一字符/Solution.java
+++ src/hash相关/q387_字符串中的第一个唯一字符/Solution.java
@@ -1,22 +1,18 @@
-package hash相关.q387_字符串中的第一个唯一字符;
-
-import java.util.HashMap;
-
-/**
- * Hash o(n)
- */
-public class Solution {
-
+class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character, Integer> count = new HashMap<>();
-        int n = s.length();
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            count.put(c, count.getOrDefault(c, 0) + 1);
+        Map<Character,Integer> map = new HashMap<Character,Integer>();
+        for(Character c : s.toCharArray()) {
+            Integer count = map.get(c);
+            if(count == null) {
+                count = 1;
+                map.put(c,count);
+            } else {
+                count = count + 1;
+                map.replace(c,count);
+            }
         }
-
-        for (int i = 0; i < n; i++) {
-            if (count.get(s.charAt(i)) == 1) {
+        for(int i = 0;i<s.length();i++) {
+            if(map.get(s.charAt(i)) == 1) {
                 return i;
             }
         }
