f87136244d59a80533a97173404974d34a78436c
zujinhao <smallzoo@foxmail.com>
2020-09-30 08:41:51 +0800

    q11

--- src/双指针遍历/q11_盛最多水的容器/Solution.java
+++ src/双指针遍历/q11_盛最多水的容器/Solution.java
@@ -3,30 +3,22 @@ package 双指针遍历.q11_盛最多水的容器;
 /**
  * 双指针遍历 o(n)
  */
-public class Solution {
+class Solution {
     public int maxArea(int[] height) {
-        if (height.length < 2) {
-            return 0;
-        }
         int left = 0;
         int right = height.length - 1;
-        int result = 0;
-        while (right > left) {
-            int c = (Math.min(height[right], height[left])) * (right - left);
-            if (c >= result) {
-                result = c;
-            }
-            if (height[left] < height[right]) {
+        int maxArea = 0;
+        while(left < right) {
+            int curArea = (right - left) * Math.min(height[left],height[right]);
+            maxArea = Math.max(maxArea, curArea);
+
+            if(height[left] < height[right]) {
                 left++;
             } else {
                 right--;
             }
         }
-        return result;
+        return maxArea;
     }
 
-    public static void main(String[] args) {
-        int[] a = new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7};
-        System.out.println(new Solution().maxArea(a));
-    }
 }
