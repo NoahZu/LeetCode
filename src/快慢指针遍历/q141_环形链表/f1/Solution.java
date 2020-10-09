a999049bd47f3d0a1fa160ad3369d9df5c38a9de
Noah <smallzoo@foxmail.com>
2020-10-09 09:12:23 +0800

    Update Solution.java

--- src/快慢指针遍历/q141_环形链表/f1/Solution.java
+++ src/快慢指针遍历/q141_环形链表/f1/Solution.java
@@ -3,21 +3,29 @@ package 快慢指针遍历.q141_环形链表.f1;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * 哈希表 o(n)
- */
 public class Solution {
-
     public boolean hasCycle(ListNode head) {
-        Set<ListNode> nodesSeen = new HashSet<>();
-        while (head != null) {
-            if (nodesSeen.contains(head)) {
-                return true;
-            } else {
-                nodesSeen.add(head);
-            }
-            head = head.next;
+        ListNode slowNode = head;
+        ListNode fastNode = head;
+
+        if(head == null || head.next == null) {
+            return false;
         }
+
+        while(slowNode != null && fastNode != null) {
+            slowNode = slowNode.next;
+            if(fastNode.next == null) {
+                fastNode = null;
+            } else {
+                fastNode = fastNode.next.next;
+            }
+
+            if(slowNode == fastNode) {
+                return true;
+            }
+        }
+
         return false;
+
     }
 }
