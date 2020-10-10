aa579b8aad7ca04f14859552a78f802012cb05bd
Noah <smallzoo@foxmail.com>
2020-10-10 10:51:15 +0800

    Update Solution.java

--- src/快慢指针遍历/q202_快乐数/Solution.java
+++ src/快慢指针遍历/q202_快乐数/Solution.java
@@ -3,27 +3,28 @@ package 快慢指针遍历.q202_快乐数;
 /**
  * 快慢指针，思想同q141判断是否有环，用快慢指针找出循环终止条件 o(n)
  */
-public class Solution {
-
-    private int bitSquareSum(int n) {
-        int sum = 0;
-        while (n > 0) {
-            int bit = n % 10;
-            sum += bit * bit;
-            n = n / 10;
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> container = new HashSet<Integer>();
+        int current = n;
+        while(true) {
+            current = getNext(current);
+            if(current == 1) {
+                return true;
+            }
+            if(container.contains(current)) {
+                return false;
+            }
+            container.add(current);
         }
-        return sum;
     }
 
-    public boolean isHappy(int n) {
-        int slow = n;
-        int fast = n;
-        do {
-            slow = bitSquareSum(slow);
-            fast = bitSquareSum(fast);
-            fast = bitSquareSum(fast);
-        } while (slow != fast);
-
-        return slow == 1;
+    int getNext(int n) {
+        int result = 0;
+        while(n > 0) {
+            result = result + (n % 10) * (n % 10);
+            n/=10;
+        }
+        return result;
     }
 }
