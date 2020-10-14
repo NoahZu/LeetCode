f200fb3ed8c8b371b56916d0a5d913c2440e30fd
Noah <smallzoo@foxmail.com>
2020-10-14 09:48:30 +0800

    Update Solution.java

--- src/字符串操作/q14_最长公共前缀/Solution.java
+++ src/字符串操作/q14_最长公共前缀/Solution.java
@@ -3,33 +3,43 @@ package 字符串操作.q14_最长公共前缀;
 /**
  * 水平扫描 o(n)
  */
-public class Solution {
+class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) {
+        if(strs.length == 0) {
             return "";
         }
-        if (strs.length == 1) {
-            return strs[0];
-        }
-        String pre = "";
-        int i = 0;
-        while (true) {
-            if (strs[0].length() == i) {
-                return pre;
+
+        String result = strs[0];
+        for(int i = 0;i<strs.length;i++) {
+            result = commonHead(result,strs[i]);
+            if(result.isEmpty()) {
+                break;
             }
-            char temp = strs[0].charAt(i);
-            for (int k = 1; k < strs.length; k++) {
-                if (strs[k].length() == i || temp != strs[k].charAt(i)) {
-                    return pre;
-                }
-            }
-            pre += temp;
-            i++;
         }
+        return result;
     }
 
-    public static void main(String[] args) {
-        String[] s = new String[]{"c", "c"};
-        System.out.println(new Solution().longestCommonPrefix(s));
+    String commonHead(String a,String b) {
+        if(a.equals(b)) {
+            return a;
+        }
+        if(a.isEmpty() || b.isEmpty()) {
+            return "";
+        }
+
+        char[] aChars = a.toCharArray();
+        char[] bChars = b.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0;i<Math.min(aChars.length,bChars.length);i++) {
+            if(aChars[i] == bChars[i]) {
+                sb.append(aChars[i]);
+            } else {
+                break;
+            }
+        } 
+
+        return sb.toString();
+        
     }
 }
