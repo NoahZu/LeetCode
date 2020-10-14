caf81f0a5b93c55223315ba791286cc8261dfd20
Noah <smallzoo@foxmail.com>
2020-10-14 17:38:02 +0800

    Update Solution.java

--- src/字符串操作/q763_划分字母区间/Solution.java
+++ src/字符串操作/q763_划分字母区间/Solution.java
@@ -6,26 +6,25 @@ import java.util.List;
 /**
  * 先存储每个字母最后出现的位置，最后遍历一次 o(n)
  */
-public class Solution {
+class Solution {
 
-    public List<Integer> partitionLabels(String S) {
-        int[] last = new int[26];
-        for (int i = 0; i < S.length(); ++i) {
-            last[S.charAt(i) - 'a'] = i;
+    public static List<Integer> partitionLabels(String S) {
+        int[] charLastIndex = new int[26];
+
+        for(int i = 0;i<S.length();i++) {
+            charLastIndex[S.charAt(i) - 'a'] = i;
         }
-        int j = 0, anchor = 0;
-        List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < S.length(); ++i) {
-            j = Math.max(j, last[S.charAt(i) - 'a']);
-            if (i == j) {
-                ans.add(i - anchor + 1);
+        int anchor = 0,j = 0;
+        List<Integer> ans = new ArrayList<Integer>();
+        for(int i = 0;i<S.length();i++) {
+            j = Math.max(j,charLastIndex[S.charAt(i) - 'a']);
+
+            if(i == j) {
+                ans.add(j-anchor+1);
                 anchor = i + 1;
             }
         }
         return ans;
     }
 
-    public static void main(String[] args) {
-        new Solution().partitionLabels("abccaddbeffe");
-    }
 }
