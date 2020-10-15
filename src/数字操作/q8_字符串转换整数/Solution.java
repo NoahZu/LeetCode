50018d74ca22e89862e1174ac6021cb1a3e1dc51
Noah <smallzoo@foxmail.com>
2020-10-15 22:12:11 +0800

    Update Solution.java

--- src/数字操作/q8_字符串转换整数/Solution.java
+++ src/数字操作/q8_字符串转换整数/Solution.java
@@ -3,38 +3,39 @@ package 数字操作.q8_字符串转换整数;
 /**
  * o(n) 重点还是判断溢出
  */
-public class Solution {
-
-    public int myAtoi(String str) {
-        str = str.trim();
-        if (str.length() < 1) {
+class Solution {
+    public int myAtoi(String s) {
+        s = s.trim();
+        if(s.isEmpty()) {
             return 0;
         }
-        boolean negative = false;
-        if (str.charAt(0) == '-') {
-            negative = true;
-            str = str.substring(1);
-        } else if (str.charAt(0) == '+') {
-            str = str.substring(1);
-        }
 
-        int rs = 0;
-        for (int i = 0; i < str.length(); i++) {
-            char t = str.charAt(i);
-            if (Character.isDigit(t)) {
-                int temp = rs * 10 - '0' + t;
-                if ((temp - t + '0') / 10 != rs || temp < 0) {
-                    return negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        char firstChar = s.charAt(0);
+        if(firstChar != '-' && firstChar != '+' && (firstChar < '0' || firstChar > '9')) {
+            return 0;
+        }
+        char[] charArray;
+        if(firstChar == '-' || firstChar == '+') {
+            charArray = s.substring(1,s.length()).toCharArray();
+        } else {
+            charArray = s.toCharArray();
+        }
+        int result = 0;
+        for(char a : charArray) {
+            if(a >= '0' && a <= '9') {
+                if(result > Integer.MAX_VALUE / 10 || result * 10 > Integer.MAX_VALUE - (a - '0')) {
+                    if(firstChar == '-') {
+                        return Integer.MIN_VALUE;
+                    } else {
+                        return Integer.MAX_VALUE;
+                    }
                 }
-                rs = temp;
+                result = result * 10 + a - '0';
             } else {
                 break;
             }
         }
-        return negative ? -rs : rs;
-    }
 
-    public static void main(String[] args) {
-        System.out.println(new Solution().myAtoi("2147483648"));
+        return firstChar == '-' ? -result : result;
     }
 }
