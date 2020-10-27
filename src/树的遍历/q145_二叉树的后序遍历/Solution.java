0177c41cefc011d79ad1c0a6479a3574d0a653e7
Noah <smallzoo@foxmail.com>
2020-10-27 13:43:01 +0800

    Update Solution.java

--- src/树的遍历/q145_二叉树的后序遍历/Solution.java
+++ src/树的遍历/q145_二叉树的后序遍历/Solution.java
@@ -7,25 +7,17 @@ import java.util.Stack;
 /**
  * 非递归法 o(n)
  */
-public class Solution {
+class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<>();
-        LinkedList<Integer> rs = new LinkedList<>();
-        if (root == null) {
-            return rs;
+        List<Integer> result = new ArrayList<Integer>();
+        if(root == null) {
+            return result;
         }
-        stack.push(root);
 
-        while (!stack.empty()) {
-            TreeNode temp = stack.pop();
-            rs.addFirst(temp.val);
-            if (temp.left != null) {
-                stack.push(temp.left);
-            }
-            if (temp.right != null) {
-                stack.push(temp.right);
-            }
-        }
-        return rs;
+        result.addAll(postorderTraversal(root.left));
+        result.addAll(postorderTraversal(root.right));
+        result.add(root.val);
+
+        return result;
     }
 }
