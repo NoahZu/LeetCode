42cc1621565ecb5406c3fcd317ca273cdc4405ec
Noah <smallzoo@foxmail.com>
2020-10-28 10:05:00 +0800

    Update Solution.java

--- src/堆相关/q215_数组中的第K个最大元素/Solution.java
+++ src/堆相关/q215_数组中的第K个最大元素/Solution.java
@@ -5,19 +5,17 @@ import java.util.PriorityQueue;
 /**
  * 利用大根堆实现 o(n*log(k))
  */
-public class Solution {
-
+class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> heap =
-                new PriorityQueue<>((n1, n2) -> n1 - n2);
+         int findCount = nums.length - k;
+         PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+         for(int i = 0;i<nums.length;i++) {
+            queue.add(nums[i]);
+         }
 
-        for (int n: nums) {
-            heap.add(n);
-            if (heap.size() > k){
-                heap.poll();
-            }
-        }
-
-        return heap.poll();
+         for(int i = 0;i<findCount;i++) {
+             queue.poll();
+         }
+         return queue.poll();
     }
 }
